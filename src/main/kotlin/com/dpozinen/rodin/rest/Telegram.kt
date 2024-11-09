48566327e96af071c542c25004947091300dfb54
dpozinen <dpozinen@gmail.com>
2024-11-09 11:46:43 +0100

    handle send error

--- src/main/kotlin/com/dpozinen/rodin/rest/Telegram.kt
+++ src/main/kotlin/com/dpozinen/rodin/rest/Telegram.kt
@@ -1,14 +1,19 @@
 package com.dpozinen.rodin.rest
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.BodyInserters
+import org.springframework.web.reactive.function.BodyInserters.fromValue
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
 
 @Service
 class Telegram(private val webclient: WebClient) {
+
+    val log: Logger = LoggerFactory.getLogger(javaClass)
 
     suspend fun getUpdates(offset: Long?): GetUpdatesResponse {
         return webclient.get()
@@ -26,9 +31,10 @@ class Telegram(private val webclient: WebClient) {
         text: String,
         markdown: Boolean = false,
         hidePreview: Boolean = true,
-    ) = webclient.post().uri("/sendMessage")
-        .body(BodyInserters.fromValue(
-            """
+    ) = runCatching {
+        webclient.post().uri("/sendMessage")
+            .body(fromValue(
+                """
                     {
                         "chat_id": "$chatId",
                         "text": "$text",
@@ -38,9 +44,10 @@ class Telegram(private val webclient: WebClient) {
                         ${markdown.takeIf { it }?.let { "\"parse_mode\": \"MarkdownV2\"" } ?: ""}
                     }
                 """.trimIndent()
-        ))
-        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-        .retrieve()
-        .awaitBody<SendMessageResponse>()
+            ))
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .retrieve()
+            .awaitBody<SendMessageResponse>()
+    }.onFailure { log.warn("Failed to send message: $text", it) }
 
 }
