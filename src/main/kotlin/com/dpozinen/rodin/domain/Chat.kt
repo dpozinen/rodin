d4fe8e3c501c9f9f954fbdb984d436a610600bf1
dpozinen <dpozinen@gmail.com>
2025-03-29 09:24:49 +0100

    support multiple languages

--- src/main/kotlin/com/dpozinen/rodin/domain/Chat.kt
+++ src/main/kotlin/com/dpozinen/rodin/domain/Chat.kt
@@ -3,8 +3,20 @@ package com.dpozinen.rodin.domain
 class Chat(
 
     val id: String,
-    var cursor: Int,
-    var wordCount: Short,
+    var currentLanguage: ChatLanguage,
+    var cursors: Map<ChatLanguage, Cursor>,
     var command: ChatCommand
 
-)
+) {
+    constructor(chatId: String) : this(chatId, ChatLanguage.FR, mapOf(
+        ChatLanguage.FR to Cursor(5, 0, ChatLanguage.FR),
+        ChatLanguage.ES to Cursor(5, 0, ChatLanguage.ES),
+    ), ChatCommand.NONE)
+
+    class Cursor(
+        var wordCount: Short,
+        var cursor: Int,
+        var language: ChatLanguage,
+    )
+
+}
